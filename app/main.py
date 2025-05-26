0ef7ff3ab57cec51cb26d3a7d4a4900eed60aa1b
Marozyk <marcin.rozycki@vp.pl>
2025-05-26 20:34:55 +0200

    Sending PONG

--- app/main.py
+++ app/main.py
@@ -4,7 +4,10 @@ import socket  # noqa: F401
 def main():
 
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    connection, _ = server_socket.accept()
+
+    connection.sendall(b"+PONG\r\n")
+    
 
 
 if __name__ == "__main__":
