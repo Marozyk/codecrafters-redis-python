066bb386083a249d710c05b49c64d2abda49af68
Marozyk <marcin.rozycki@vp.pl>
2025-05-26 21:21:24 +0200

    Multiple PONG responses

--- app/main.py
+++ app/main.py
@@ -1,12 +1,24 @@
 import socket  # noqa: F401
 
 
+def init_server(address, port):
+    server_socket = socket.create_server((address, port), reuse_port=True)
+    connection, address = server_socket.accept()
+    return connection, address
+
 def main():
 
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    connection, _ = server_socket.accept()
-
-    connection.sendall(b"+PONG\r\n")
+    connection, _ =init_server("localhost", 6379)
+    while True:
+        data = connection.recv(1024)
+        stringdata = data.decode('utf-8').strip()
+        if not data:
+            break
+        if "PING" in stringdata:
+            connection.sendall(b"+PONG\r\n")
+        else:
+            break
+    
     
 
 
